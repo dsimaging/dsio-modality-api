db877936a8845d092da9d846117395c90b2d5e38
John Goyette <John.Goyette@dentsplysirona.com>
2021-03-20 13:28:38 -0400

    dotnet: Added WpfSample

--- sdk/dotnet/build.ps1
+++ sdk/dotnet/build.ps1
@@ -2,3 +2,4 @@ dotnet build .\lib\DSIO.Modality.Api.Sdk.Types\DSIO.Modality.Api.Sdk.Types.cspro
 dotnet build .\lib\DSIO.Modality.Api.Sdk.Client\DSIO.Modality.Api.Sdk.Client.csproj
 
 dotnet build .\samples\ConsoleApp\ConsoleApp.csproj
+dotnet build .\samples\WpfSample\WpfSample.csproj
