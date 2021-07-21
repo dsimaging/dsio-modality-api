319a812b6a4d6c9d29b17f8a19f07ef74598c8b2
John Goyette <John.Goyette@dentsplysirona.com>
2021-07-20 22:00:44 -0400

    Added version info to build script

--- sdk/dotnet/build.ps1
+++ sdk/dotnet/build.ps1
@@ -1,5 +1,33 @@
-dotnet build --configuration Release .\lib\DSIO.Modality.Api.Sdk.Types\DSIO.Modality.Api.Sdk.Types.csproj
-dotnet build --configuration Release .\lib\DSIO.Modality.Api.Sdk.Client\DSIO.Modality.Api.Sdk.Client.csproj
+# Define build properties
+$VersionPrefix= "0.1"
+$VersionSuffix= ""
+$Configuration = "Release"
 
-dotnet build --configuration Release .\samples\ConsoleApp\ConsoleApp.csproj
-dotnet build --configuration Release .\samples\WpfSample\WpfSample.csproj
+# Get version from git tag
+$git_description = git describe --long --tags --always
+if ($null -ne $git_description) {
+    # Parse for tag and commit
+    if ($git_description -match '[v](.*?)-(.*)') {
+        # extract version from tag and suffix from commit/extras
+        $VersionPrefix = $Matches[1]
+        $VersionSuffix = $Matches[2]
+    } else {
+        # version tag not found - just set the suffix to the git description
+        $VersionSuffix = $git_description;
+    }
+}
+
+Write-Host "Cleaning..."
+dotnet clean --configuration $Configuration
+
+Write-Host "Compiling SDK Library Version: $VersionPrefix-$VersionSuffix"
+dotnet build --configuration $Configuration -p:VersionPrefix=$VersionPrefix -p:VersionSuffix=$VersionSuffix `
+        .\lib\DSIO.Modality.Api.Sdk.Types\DSIO.Modality.Api.Sdk.Types.csproj
+dotnet build --configuration $Configuration -p:VersionPrefix=$VersionPrefix -p:VersionSuffix=$VersionSuffix `
+        .\lib\DSIO.Modality.Api.Sdk.Client\DSIO.Modality.Api.Sdk.Client.csproj
+
+Write-Host "Compiling SDK Samples Version: $VersionPrefix-$VersionSuffix"
+dotnet build --configuration $Configuration -p:VersionPrefix=$VersionPrefix -p:VersionSuffix=$VersionSuffix `
+        .\samples\ConsoleApp\ConsoleApp.csproj
+dotnet build --configuration $Configuration -p:VersionPrefix=$VersionPrefix -p:VersionSuffix=$VersionSuffix `
+        .\samples\WpfSample\WpfSample.csproj
